2371e84a05c24d53f9e845829702531a678701e2
yungyuchen521 <yungyuchen521@gmail.com>
2023-12-15 00:13:15 +0800

    ES Individual

--- individual.py
+++ individual.py
@@ -2,16 +2,16 @@ from copy import deepcopy
 
 import numpy as np
 
-from .fc import FC
+from .fc import BaseFc, GaFc, EsFc
 
 
-class Individual:
-    def __init__(self, dim_list: list[int] = [], **ga_operator_config):
+class BaseIndividual:
+    def __init__(self, dim_list: list[int] = [], **fc_kwargs):
         """
-            dim_list: [ in_dim, hidden_dim1, hidden_dim2, ..., out_dim ]
+        dim_list: [ in_dim, hidden_dim1, hidden_dim2, ..., out_dim ]
         """
 
-        self.layers: list[FC] = []
+        self.layers: list[BaseFc] = []
         self.loss: float = None
         self.acc: float = None
         self.age: int = 0
@@ -23,19 +23,19 @@ class Individual:
 
             if i < len(dim_list) - 2:
                 # hidden layer
-                activation = FC.ACT_RELU
+                activation = BaseFc.ACT_RELU
             elif out_dim == 1:
                 # output layer
-                activation = FC.ACT_SIGMOID
+                activation = BaseFc.ACT_SIGMOID
             else:
                 # output layer
-                activation = FC.ACT_SOFTMAX
+                activation = BaseFc.ACT_SOFTMAX
 
-            fc = FC.build_from_dim(
+            fc = self._build_fc(
                 in_dim=in_dim,
                 out_dim=out_dim,
                 activation=activation,
-                **ga_operator_config,
+                **fc_kwargs,
             )
             self.layers.append(fc)
 
@@ -74,24 +74,39 @@ class Individual:
         assert self.loss is None  # loss should be calculated only once
         self.loss = loss
 
-    def add_layer(self, layer: FC):
+    def add_layer(self, layer: BaseFc):
         assert (len(self.layers) == 0) or (self.out_dim == layer.in_dim)
         self.layers.append(layer)
+
+    def mutate(self):
+        raise NotImplementedError
+
+    def recombine(self, other) -> tuple:
+        raise NotImplementedError
+
+    def _build_fc(self, in_dim: int, out_dim: int, activation: str, **kwargs) -> BaseFc:
+        raise NotImplementedError
+
+
+class GaIndividual(BaseIndividual):
+    def __init__(self, dim_list: list[int] = [], **ga_operator_config):
+        super().__init__(dim_list, **ga_operator_config)
+        self.layers: list[GaFc]
 
     def mutate(self):
         """update in place"""
         for fc in self.layers:
             fc.mutate()
 
-        # need reevaluation 
+        # need reevaluation
         self.acc = None
         self.loss = None
 
     def recombine(self, other) -> tuple:
-        assert isinstance(other, Individual)
+        assert isinstance(other, GaIndividual)
         assert len(self.layers) == len(other.layers)
 
-        child_a, child_b = Individual(), Individual()
+        child_a, child_b = GaIndividual(), GaIndividual()
 
         for self_layer, other_layer in zip(self.layers, other.layers):
             fc_a, fc_b = self_layer.recombine(other_layer)
@@ -99,3 +114,32 @@ class Individual:
             child_b.add_layer(fc_b)
 
         return child_a, child_b
+
+    def _build_fc(self, in_dim: int, out_dim: int, activation: str, **kwargs) -> GaFc:
+        return GaFc.build_from_dim(
+            in_dim=in_dim,
+            out_dim=out_dim,
+            activation=activation,
+            **kwargs,
+        )
+
+
+class EsIndividual(BaseIndividual):
+    def __init__(self, dim_list: list[int] = [], **kwargs):
+        super().__init__(dim_list, **kwargs)
+        self.layers: list[EsFc]
+
+    def mutate(self):
+        child = EsIndividual()
+        for layer in self.layers:
+            child.add_layer(layer.mutate())
+
+        return child
+
+    def _build_fc(self, in_dim: int, out_dim: int, activation: str, **kwargs) -> EsFc:
+        return EsFc.build_from_dim(
+            in_dim=in_dim,
+            out_dim=out_dim,
+            activation=activation,
+            **kwargs,
+        )
